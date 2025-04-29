69f0f35226d010d6e624bb76ce6badacacd8a7b4
Akhtar Parveen <akhujapbca@gmail.com>
2025-04-30 01:10:17 +0530

    feat: return 0 for empty string

--- test/tddstringCalculator.test.js
+++ test/tddstringCalculator.test.js
@@ -1,7 +1,11 @@
 const add = require("../src/tddstringCalculator");
 
 describe("String Calculator", () => {
-  test("returns 0 for empty string", () => {
-    expect(add("")).toBe(0);
-  });
+
+    describe("Test Case 1: Empty String", () => {
+        test("returns 0 for empty string", () => {
+            expect(add("")).toBe(0);
+        });
+    });
+
 });
